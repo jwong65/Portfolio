5e21dedabe98835fca3c1da94a382ac0efd1751d
jwong65 <jason.wong6507@gmail.com>
2023-02-09 20:31:55 -0500

    Added components and imported the Card/Box for Card.js

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+
+export default function Card() {
+  return (
+    <div>Card</div>
+  )
+}
